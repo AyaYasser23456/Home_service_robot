47541292f3a814184e91ec7eace1fb1cd7ef4278
Aya Yasser <Ayaabotaleb25@gamil.com>
2021-11-29 22:52:03 +0200

    final

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -6,7 +6,7 @@ xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_gaz
 sleep 15
 xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:="/home/workspace/catkin_ws/src/map/maps/map1.yaml"" &
 sleep 10
-xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation1.launch rviz_config_file:="/home/workspace/catkin_ws/src/rvizConfig/navigation1.rviz"" &
+xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch rviz_config_file:="/home/workspace/catkin_ws/src/rvizConfig/navigation1.rviz"" &
 sleep 15
 xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch pick_objects pick_objects.launch" &
 
